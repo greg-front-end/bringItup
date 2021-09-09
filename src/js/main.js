317f94c14305e4411f7ed364bec115c217d379c9
Martin <martinstudyweb@gmail.com>
2021-09-09 12:58:37 +0300

    create ShowInfo pattern

--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,7 @@ import MainSlider from "./modules/slider/slider-main";
 import MiniSlider from "./modules/slider/slider-mini";
 import Difference from "./modules/difference";
 import Form from "./modules/form";
+import ShowInfo from "./modules/showInfo";
 
 window.addEventListener('DOMContentLoaded', () => {
     const slider = new MainSlider({btns: '.next', container: '.page'});
@@ -46,4 +47,6 @@ window.addEventListener('DOMContentLoaded', () => {
     new Difference('.officerold', '.officernew', '.officer__card-item').init();
 
     new Form('.form').init();
+
+    new ShowInfo('.plus__content').init();
 });
